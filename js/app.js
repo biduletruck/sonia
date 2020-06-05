9c0833999e3f5c4368507cb08429d787f7328216
yann <clementyann@free.fr>
2020-06-05 15:21:15 +0200

    mise a jour structure et js

--- js/app.js
+++ js/app.js
@@ -3,30 +3,47 @@ $(document).ready(function(){
     var current_fs, next_fs, previous_fs; //fieldsets
     var opacity;
 
-    $(".next").click(function(){
+     $(".next").click(function(){
 
         current_fs = $(this).parent();
         next_fs = $(this).parent().next();
 
-//Add Class Active
-        $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
+
+        $($(this).parent()).each(function(){
+            var totalPoints = 0;
+            $(this).find('input[type = number]').each(function(){
+                totalPoints += parseInt($(this).val()); //<==== a catch  in here !! read below
+            });
+
+            if(totalPoints == 12){
+                //Add Class Active
+                $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
 
 //show the next fieldset
-        next_fs.show();
+                next_fs.show();
 //hide the current fieldset with style
-        current_fs.animate({opacity: 0}, {
-            step: function(now) {
+                current_fs.animate({opacity: 0}, {
+                    step: function(now) {
 // for making fielset appear animation
-                opacity = 1 - now;
+                        opacity = 1 - now;
 
-                current_fs.css({
-                    'display': 'none',
-                    'position': 'relative'
+                        current_fs.css({
+                            'display': 'none',
+                            'position': 'relative'
+                        });
+                        next_fs.css({'opacity': opacity});
+                    },
+                    duration: 600
                 });
-                next_fs.css({'opacity': opacity});
-            },
-            duration: 600
+            }else{
+                alert("vous avez renseigné " + totalPoints + " point(s). Le total doit être de 12 pour pousuivre")
+
+            }
+            //$("#result").text(totalPoints);
+            //alert(totalPoints);
         });
+
+
     });
 
     $(".previous").click(function(){
